67f21ec085a9341abd77483bf91d32b285ae5d71
Arseniy Tsinzerling <ars.tsin@gmail.com>
2023-06-01 13:56:27 -0400

    graph displays total value

--- reactapp/src/pages/Dashboard.js
+++ reactapp/src/pages/Dashboard.js
@@ -1,5 +1,3 @@
-/*import CryptoChart from './CryptoChart';*/
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
@@ -9,56 +7,80 @@ import './Dashboard.css';
 const holdings = [
     {
         date: '2023-01-01',
-        Bitcoin: 400,
-        Ethereum: 3000,
+        bitcoin: 400,
+        ethereum: 3000,
     },
     {
         date: '2023-02-01',
-        Bitcoin: 410,
-        Ethereum: 3100,
-        Litecoin: 190,
+        bitcoin: 410,
+        ethereum: 3100,
+        ripple: 190,
     },
     {
         date: '2023-03-01',
-        Bitcoin: 500,
-        Ethereum: 3150,
-        Litecoin: 210,
+        bitcoin: 500,
+        ethereum: 3150,
+        ripple: 210,
     },
     {
         date: '2023-05-01',
-        Bitcoin: 500,
-        Ethereum: 3100,
-        Litecoin: 215,
+        bitcoin: 500,
+        ethereum: 3100,
+        ripple: 215,
     }
 ];
 
-
-const CryptoChart = ({ startDate }) => {
+const CryptoChart = () => {
     const [data, setData] = useState([]);
+    const [holdingsData, setHoldingsData] = useState(holdings); // Use holdings as state
+
     const [totalValue, setTotalValue] = useState(0);
+    const [coinDetails, setCoinDetails] = useState([]);
+
+    // Extract coins and start date from holdings
+    const coins = [...new Set(holdingsData.flatMap(holding => Object.keys(holding).filter(key => key !== 'date')))];
+    const startDate = holdingsData.reduce((minDate, holding) => holding.date < minDate ? holding.date : minDate, holdingsData[0].date);
 
     useEffect(() => {
         const fetchData = async () => {
-            const coins = ['bitcoin', 'ethereum', 'ripple']; // replace with your coins
+            // Order the holdings data by date
+            holdingsData.sort((a, b) => moment(a.date).isBefore(moment(b.date)) ? -1 : 1);
+
+            let currentHoldings = {};
+
+            // Initialize chartData with an array covering each date from startDate to today
             let chartData = [];
+            for (let m = moment(startDate); m.isSameOrBefore(moment(), 'day'); m.add(1, 'days')) {
+                chartData.push({ date: m.format('YYYY-MM-DD') });
+            }
 
             for (const coin of coins) {
-                const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${coin}/market_chart`, {
+                // Fetch price data for the coin
+                const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${coin.toLowerCase()}/market_chart`, {
                     params: {
                         vs_currency: 'usd',
                         days: moment().diff(moment(startDate), 'days')
                     }
                 });
 
-                response.data.prices.forEach((item, index) => {
+                response.data.prices.forEach((item) => {
                     const date = moment(item[0]).format('YYYY-MM-DD');
                     const price = item[1];
 
-                    if (!chartData[index]) {
-                        chartData[index] = { date };
+                    // Find the corresponding holding for the current date
+                    const newHolding = holdingsData.find(holding => holding.date === date);
+
+                    // Update currentHoldings if newHolding exists
+                    if (newHolding) {
+                        currentHoldings = { ...currentHoldings, ...newHolding };
                     }
 
-                    chartData[index][coin] = price;
+                    const quantity = currentHoldings[coin] || 0;
+
+                    // Find the corresponding data item for the current date
+                    const dataItem = chartData.find(dataItem => dataItem.date === date);
+                    dataItem[coin] = price * quantity; // Calculate value of holding
+                    dataItem.total = (dataItem.total || 0) + dataItem[coin]; // Update total value
                 });
             }
 
@@ -66,15 +88,25 @@ const CryptoChart = ({ startDate }) => {
         };
 
         fetchData();
-    }, [startDate]);
+    }, [startDate, holdingsData, coins]);
+
+
 
     const CustomTooltip = ({ active, payload, label }) => {
         if (active && payload && payload.length) {
             const total = payload.reduce((acc, current) => acc + current.value, 0);
             setTotalValue(total.toFixed(2));
+
+            const coinDetail = payload.map((coin) => ({
+                name: coin.name,
+                value: coin.value,
+                quantity: coin.payload[coin.dataKey]
+            }));
+            setCoinDetails(coinDetail);
+
             return (
-                <div className="custom-tooltip" > {/*style={{ backgroundColor: '#fff', padding: '3px', border: '1px solid #ccc', color: '#050404' }}*/}
-                    {/*<p>{`Date: ${label}`}</p>*/}
+                <div className="custom-tooltip">
+                    <p>{`Date: ${label}`}</p>
                     <p>{`Total value: ${total.toFixed(2)} USD`}</p>
                 </div>
             );
@@ -83,9 +115,8 @@ const CryptoChart = ({ startDate }) => {
         return null;
     };
 
-
     return (
-        <div class='display-graph-data'>
+        <div className='display-graph-data'>
             <LineChart
                 width={500}
                 height={300}
@@ -96,12 +127,15 @@ const CryptoChart = ({ startDate }) => {
                 <YAxis />
                 <CartesianGrid strokeDasharray="3 3" />
                 <Tooltip content={<CustomTooltip />} />
-                <Line type="monotone" dataKey="bitcoin" stroke="#8884d8" dot={false} />
-                <Line type="monotone" dataKey="ethereum" stroke="#82ca9d" dot={false} />
-                <Line type="monotone" dataKey="ripple" stroke="#ffc658" dot={false} />
+                <Line type="monotone" dataKey="total" stroke="#8884d8" dot={false} /> {/* Show total value */}
             </LineChart>
             <div>
                 <p>{`Total value: ${totalValue} USD`}</p>
+                {coinDetails.map((coin) => (
+                    <p key={coin.name}>
+                        {`${coin.name}: Quantity = ${coin.quantity}, Total Value = ${coin.value} USD`}
+                    </p>
+                ))}
             </div>
         </div>
     );
@@ -111,7 +145,7 @@ const Dashboard = () => {
     return (
         <div>
             <h1>Dashboard</h1>
-            <CryptoChart startDate="2022-01-01" />
+            <CryptoChart />
         </div>
     );
 };
