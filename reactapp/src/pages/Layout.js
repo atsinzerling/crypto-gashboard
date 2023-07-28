bfea84b8c8a93ec95276d4539d98ca02023c2ce6
arseniy tsinzerling <ars.tsin@gmail.com>
2023-07-27 23:17:55 -0400

    initial config of mui

--- reactapp/src/pages/Layout.js
+++ reactapp/src/pages/Layout.js
@@ -1,9 +1,23 @@
 import { Outlet, Link } from "react-router-dom";
 import './Layout.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1976d2',
+        },
+        secondary: {
+            main: '#dc004e',
+        },
+    },
+});
+
+
 
 const Layout = () => {
     return (
-        <>
+        <ThemeProvider theme={theme}>
             <nav>
                 <ul>
                     <li>
@@ -22,7 +36,7 @@ const Layout = () => {
             </nav>
 
             <Outlet />
-        </>
+        </ThemeProvider>
     )
 };
 
