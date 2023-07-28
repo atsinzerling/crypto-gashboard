0125270f562829d4deaf0d7dbe1c8e18a4ee78ff
arseniy tsinzerling <ars.tsin@gmail.com>
2023-07-28 17:10:59 -0400

    mui for navbar

--- reactapp/src/pages/Layout.js
+++ reactapp/src/pages/Layout.js
@@ -1,6 +1,30 @@
 import { Outlet, Link } from "react-router-dom";
 import './Layout.css';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';  
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+
+const NavBarButton = (props) => {
+    const { to, children } = props;
+    return (
+        <Button
+            component={RouterLink}
+            to={to}
+            sx={{
+                color: '#fff',
+                backgroundColor: '#007BFF',
+                borderRadius: '5px',
+                textDecoration: 'none',
+                fontSize: '16px',
+                '&:hover': {
+                    backgroundColor: '#0056b3',
+                },
+            }}
+        >
+            {children}
+        </Button>
+    )
+}
 
 const theme = createTheme({
     palette: {
@@ -18,23 +42,16 @@ const theme = createTheme({
 const Layout = () => {
     return (
         <ThemeProvider theme={theme}>
-            <nav>
-                <ul>
-                    <li>
-                        <Link className="button"  to="/">Dashboard</Link>
-                    </li>
-                    <li>
-                        <Link className="button"  to="/manage">Manage Trackings</Link>
-                    </li>
-                    <li>
-                        <Link className="button" to="/test">Test</Link>
-                    </li>
-                    <li className="login">
-                        <Link className="button"  to="/login">Log in</Link>
-                    </li>
-                </ul>
-            </nav>
-
+            <AppBar position="static" color="transparent">
+                <Toolbar sx={{ justifyContent: 'space-between' }}>
+                    <Box display="flex" gap={2}>
+                        <NavBarButton to="/">Dashboard</NavBarButton>
+                        <NavBarButton to="/manage">Manage Trackings</NavBarButton>
+                        <NavBarButton to="/test">Test</NavBarButton>
+                    </Box>
+                    <NavBarButton to="/login">Log in</NavBarButton>
+                </Toolbar>
+            </AppBar>
             <Outlet />
         </ThemeProvider>
     )
